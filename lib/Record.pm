732d2b9f6683e8d8d2858c6f4a719c9aa67d8eee
stlange <ststlange@gmail.com>
2015-09-16 01:54:32 +0900

    enable to record A&G and NHK radiru.

--- lib/Record.pm
+++ lib/Record.pm
@@ -11,21 +11,16 @@ our $RTMPDUMP_PATH = '/usr/local/bin/rtmpdump';
 our $SWFEXTRACT_PATH = '/usr/local/bin/swfextract';
 our $FFMPEG_PATH = '/usr/local/bin/ffmpeg';
 
-my @REQUIRED_COMMAND = (
-    $RTMPDUMP_PATH,
-    $SWFEXTRACT_PATH,
-    $FFMPEG_PATH,
+my @REQUIRED_COMMAND = qw(
+    rtmpdump_path
+    swfextract_path
+    ffmpeg_path
 );
 
 sub new {
     my $class = shift;
     my $opts = shift;
 
-    foreach my $command (@REQUIRED_COMMAND) {
-        if( ! -x $command) {
-            die qq{can't execute $command};
-        }
-    }
 
     my $conf_file = $opts->{conf} // 'radiko.ini';
     my $config = new Config::Tiny->read($conf_file);
@@ -45,6 +40,12 @@ sub new {
 
     if( ! -d $config->{global}{savedir}) {
         mkpath $config->{global}{savedir};
+    }
+
+    foreach my $command (@REQUIRED_COMMAND) {
+        if( ! -x $config->{global}{$command}) {
+            #die qq{can't execute $command $config->{global}{$command}};
+        }
     }
 
     my $player_path = $this->_file_to_worksdir_path($config->{radiko}{player});
@@ -82,8 +83,69 @@ sub start {
     my $this = shift;
 
     my ($file_name, $station, $duration, $area) = @_;
-    my $file = $this->_file_to_worksdir_path($file_name);
 
+    if($station eq 'A&G') {
+        $this->_start_a_and_g($file_name, $duration);
+    } elsif($station =~ /\A(?:R1|R2|FM)\z/) {
+        $this->_start_radiru($file_name, $station, $duration);
+    }else {
+        $this->_start_radiko($file_name, $station, $duration, $area);
+    }
+}
+
+sub _start_radiru {
+    my $this = shift;
+    my ($file_name, $station, $duration) = @_;
+
+    my $file = $this->_file_to_worksdir_path($file_name);
+    my $url = $this->{conf}{radiru}{url};
+    my $playpath = $this->{conf}{radiru}{"playpath_$station"};
+    my $playerurl= $this->{conf}{radiru}{player_url};
+
+    $this->_debug_print($file);
+
+    my $record_command = qq{
+        $RTMPDUMP_PATH -v
+        -r "$url"
+        --playpath "$playpath"
+        --app "live"
+        -W $playerurl
+        --stop $duration
+        -o "$file"
+    };
+
+    $record_command =~ s/\n//g;
+
+    use Data::Dumper; $this->_debug_print(Dumper($record_command));
+    #use Data::Dumper; $this->_debug_print(Dumper($this->{conf}{playpath}));
+
+    my $result = system $record_command;
+}
+
+sub _start_a_and_g {
+    $this = shift;
+    my ($file_name, $duration) = @_;
+
+    my $file = $this->_file_to_worksdir_path($file_name);
+    my $url = $this->{conf}{a_and_g}{url};
+
+    my $record_command = qq{
+        $RTMPDUMP_PATH -v
+        -r "$url"
+        --live
+        --stop $duration
+        -o "$file"
+    };
+
+    $record_command =~ s/\n//g;
+
+    my $result = system $record_command;
+}
+
+sub _start_radiko {
+    my $this = shift;
+    my ($file_name, $station, $duration, $area) = @_;
+    my $file = $this->_file_to_worksdir_path($file_name);
 
     my $request = HTTP::Request->new(
         POST => $this->{conf}{radiko}{auth1_url},
@@ -158,6 +220,7 @@ sub start {
     $this->_start_record($file, $station, $duration, $areaid, $authtoken);
 }
 
+
 sub convert {
     my $this = shift;
 
@@ -194,10 +257,11 @@ sub _start_record {
     my ($file, $station, $duration, $area, $authtoken) = @_;
 
     my $playerurl = $this->{conf}{radiko}{player_url};
+    my $url = $this->{conf}{radiko}{url};
 
     my $record_command = qq{
         $RTMPDUMP_PATH -v
-        -r "rtmpe://f-radiko.smartstream.ne.jp"
+        -r "$url"
         --playpath "simul-stream.stream"
         --app "$station/_definst_"
         -W $playerurl
@@ -230,147 +294,3 @@ sub _debug_print {
 }
 
 1;
-##!/bin/sh
-#
-#date=`date '+%Y-%m-%d-%H:%M'`
-##date=`date '+%Y-%m-%d'`
-#playerurl=http://radiko.jp/player/swf/player_3.0.0.01.swf
-#home=/home/stlange/script/radiko
-#playerfile=$home/player.swf
-#keyfile=$home/authkey.png
-#
-#if [ $# -eq 3 ]; then
-#  station=$1
-#  DURATION=`expr $2 \* 60`
-#  showName=$3
-#else
-#  echo "usage : $0 station_name duration(minuites) showName"
-#  exit 1
-#fi
-#
-##
-## get player
-##
-#if [ ! -f $playerfile ]; then
-#  wget -q -O $playerfile $playerurl
-#
-#  if [ $? -ne 0 ]; then
-#    echo "failed get player"
-#    exit 1
-#  fi
-#fi
-#
-##
-## get keydata (need swftool)
-##
-#if [ ! -f $keyfile ]; then
-#  /usr/local/bin/swfextract -b 14 $playerfile -o $keyfile
-#
-#  if [ ! -f $keyfile ]; then
-#    echo "failed get keydata"
-#    exit 1
-#  fi
-#fi
-#
-#if [ -f auth1_fms ]; then
-#  rm -f auth1_fms
-#fi
-#if [ -f auth1_fms.1 ]; then
-#  rm -f auth1_fms.*
-#fi
-#
-##
-## access auth1_fms
-##
-#wget -q \
-#     --header="pragma: no-cache" \
-#     --header="X-Radiko-App: pc_1" \
-#     --header="X-Radiko-App-Version: 2.0.1" \
-#     --header="X-Radiko-User: test-stream" \
-#     --header="X-Radiko-Device: pc" \
-#     --post-data='\r\n' \
-#     --no-check-certificate \
-#     --save-headers \
-#https://radiko.jp/v2/api/auth1_fms
-#
-#if [ $? -ne 0 ]; then
-#  echo "failed auth1 process"
-#  exit 1
-#fi
-#
-##
-## get partial key
-##
-#authtoken=`perl -ne 'print $1 if(/x-radiko-authtoken: ([\w-]+)/i)' auth1_fms`
-#offset=`perl -ne 'print $1 if(/x-radiko-keyoffset: (\d+)/i)' auth1_fms`
-#length=`perl -ne 'print $1 if(/x-radiko-keylength: (\d+)/i)' auth1_fms`
-#
-#partialkey=`dd if=$keyfile bs=1 skip=${offset} count=${length} 2> /dev/null | base64`
-#echo "authtoken: ${authtoken} \noffset: ${offset} length: ${length} \npartialkey: 
-#
-#$partialkey"
-#
-#rm -f auth1_fms
-#
-#if [ -f auth2_fms ]; then
-#  rm -f auth2_fms
-#fi
-#
-#if [ -f auth2_fms.1 ]; then
-#  rm -f auth2_fms.*
-#fi
-##
-## access auth2_fms
-##
-#wget -q \
-#     --header="pragma: no-cache" \
-#     --header="X-Radiko-App: pc_1" \
-#     --header="X-Radiko-App-Version: 2.0.1" \
-#     --header="X-Radiko-User: test-stream" \
-#     --header="X-Radiko-Device: pc" \
-#     --header="X-Radiko-Authtoken: ${authtoken}" \
-#     --header="X-Radiko-Partialkey: ${partialkey}" \
-#     --post-data='\r\n' \
-#     --no-check-certificate \
-#https://radiko.jp/v2/api/auth2_fms
-#
-#if [ $? -ne 0 -o ! -f auth2_fms ]; then
-#  echo "failed auth2 process"
-#  exit 1
-#fi
-#
-#echo "authentication success"
-#areaid=`perl -ne 'print $1 if(/^([^,]+),/i)' auth2_fms`
-#echo "areaid: $areaid"
-#
-#rm -f auth2_fms
-#
-##
-## rtmpdump
-##
-##/opt/rtmpdump-2.4/rtmpdump -v \
-#/usr/local/bin/rtmpdump -v \
-#         -r "rtmpe://f-radiko.smartstream.ne.jp" \
-#         --playpath "simul-stream.stream" \
-#         --app "${station}/_definst_" \
-#         -W $playerurl \
-#         -C S:"" -C S:"" -C S:"" -C S:$authtoken \
-#         --live \
-#         --stop $DURATION \
-#         -o "$home/${1}_${date}"
-#
-##ffmpeg -y -i "/tmp/${1}_${date}" -acodec libmp3lame "/var/www/test/audio/${1}_${date}.mp3"
-##ffmpeg -y -i "./${1}_${date}" -acodec mp3 "./${1}_${date}.mp3"
-##ffmpegだと動かない
-#/usr/local/bin/ffmpeg -y -i "$home/${1}_${date}" -acodec mp3 "$home/${1}_${date}.mp3"
-#
-#if [ ! -d $home/${showName} ]; then
-#  mkdir $home/${showName}
-#  echo $home/${showName} >> $home/.gitignore
-#  
-#fi
-#
-#mv $home/${1}_${date}.mp3 $home/${showName}/${showName}_${date}.mp3
-#
-#
-#rm "$home/${1}_${date}"
--- t/Record/01_start.t
+++ t/Record/01_start.t
@@ -7,7 +7,7 @@ use Record;
 
 use Config::Tiny;
 
-{
+subtest 'rec radiko' => sub {
     my $conf_file = 'radiko.ini.test';
     my $player_file = 'player.swf';
     my $config = new Config::Tiny;
@@ -18,6 +18,7 @@ use Config::Tiny;
     $config->{radiko}{player_url} = 'http://radiko.jp/player/swf/player_3.0.0.01.swf';
     $config->{radiko}{auth1_url} = 'https://radiko.jp/v2/api/auth1_fms';
     $config->{radiko}{auth2_url} = 'https://radiko.jp/v2/api/auth2_fms';
+    $config->{radiko}{url} = 'rtmpe://f-radiko.smartstream.ne.jp';
     $config->write($conf_file);
 
     my $recorder = new Record({
@@ -29,7 +30,7 @@ use Config::Tiny;
     ok -e $player_path, 'setup player file';
     my $player_file_type = `file $player_path`;
     like $player_file_type, qr/Macromedia Flash data/, 'check player file type';
-    
+
     my $file = "test.flv";
     $recorder->start(
         $file,
@@ -41,6 +42,9 @@ use Config::Tiny;
     {
         my $size = -s "$base/$file";
         ok $size > 0, "rec ok $file";
+
+        my $flv_file_type = `file $base/$file`;
+        like $flv_file_type, qr/Macromedia Flash Video/, 'check flv file type';
     }
 
     my $savedir = $config->{global}{savedir};
@@ -59,9 +63,116 @@ use Config::Tiny;
     system "rm -rf $savedir";
     unlink $conf_file;
 
-}
+};
+
+#超A&G
+subtest 'A&G' => sub {
+    my $conf_file = 'radiko.ini.test';
+    my $config = new Config::Tiny;
+    $config->{global}{rtmpdump_path} = '/usr/local/bin/rtmpdump';
+    $config->{global}{swfextract_path} = '/usr/local/bin/swfextract';
+    $config->{global}{ffmpeg_path} = '/usr/local/bin/ffmpeg';
+   $config->{global}{workdir} = './testbase';
+    $config->{global}{savedir} = './savedir';
+    $config->{global}{debug} = 1;
+    $config->{a_and_g}{url} = 'rtmp://fms-base1.mitene.ad.jp/agqr/aandg22';
+    $config->write($conf_file);
+
+    my $recorder = new Record({
+        conf => $conf_file,
+    });
+
+    my $base = $config->{global}{workdir};
+    my $file = "test.flv";
+    $recorder->start(
+        $file,
+        'A&G',
+        1,
+    );
+
+    {
+        my $size = -s "$base/$file";
+        ok $size > 0, "rec ok $file";
+
+        my $flv_file_type = `file $base/$file`;
+        like $flv_file_type, qr/Macromedia Flash Video/, 'check flv file type';
+    }
+
+    my $savedir = $config->{global}{savedir};
+    my $dest_file = 'test.mp3';
+
+    $recorder->convert($file, $dest_file);
+
+    ok -e "$savedir/$dest_file", 'convert ok';
+    my $conv_file_type = `file $savedir/$dest_file`;
+
+    like $conv_file_type, qr/MPEG ADTS/, 'check file type';
 
 
+    system "cp testbase/proc.log .";
+    system "rm -rf $base";
+    system "rm -rf $savedir";
+    unlink $conf_file;
+
+};
+
+#NHK
+SKIP: {
+subtest 'radiru' => sub {
+    plan skip_all => 'rtmpdump version is less than 2.4';
+    my $conf_file = 'radiko.ini.test';
+    my $config = new Config::Tiny;
+    $config->{global}{rtmpdump_path} = '/usr/local/bin/rtmpdump';
+    $config->{global}{swfextract_path} = '/usr/local/bin/swfextract';
+    $config->{global}{ffmpeg_path} = '/usr/local/bin/ffmpeg';
+   $config->{global}{workdir} = './testbase';
+    $config->{global}{savedir} = './savedir';
+    $config->{global}{debug} = 1;
+    $config->{radiru}{url} = 'rtmpe://netradio-fm-flash.nhk.jp';
+    $config->{radiru}{player_url} = 'http://www3.nhk.or.jp/netradio/files/swf/rtmpe.swf';
+    $config->{radiru}{playpath_R1} = 'NetRadio_R1_flash@63346';
+    $config->{radiru}{playpath_R2} = 'NetRadio_R2_flash@63342';
+    $config->{radiru}{playpath_FM} = 'NetRadio_FM_flash@63343';
+    $config->write($conf_file);
+
+    my $recorder = new Record({
+        conf => $conf_file,
+    });
+
+    my $base = $config->{global}{workdir};
+    my $file = "test.flv";
+    ok $recorder->start(
+        $file,
+        'R1',
+        1,
+    ), 'start ok';
+
+    {
+        my $size = -s "$base/$file";
+        ok $size > 0, "rec ok $file";
+
+        my $flv_file_type = `file $base/$file`;
+        like $flv_file_type, qr/Macromedia Flash Video/, 'check flv file type';
+    }
+
+    my $savedir = $config->{global}{savedir};
+    my $dest_file = 'test.mp3';
+
+    #$recorder->convert($file, $dest_file);
+
+#    ok -e "$savedir/$dest_file", 'convert ok';
+#    my $conv_file_type = `file $savedir/$dest_file`;
+#
+#    like $conv_file_type, qr/MPEG ADTS/, 'check file type';
+#
+#
+#    system "cp testbase/proc.log .";
+#    system "rm -rf $base";
+#    system "rm -rf $savedir";
+#    unlink $conf_file;
+
+};
+};
 
 
 done_testing;
